3aeb5613f7d65e7573063e11706d1d8e2a155a7a
Leng <lengzhang@live.com>
2021-01-28 14:06:26 -0800

    Added checkAnswer

--- app/src/main/java/com/lengzhang/android/geoquiz/MainActivity.kt
+++ app/src/main/java/com/lengzhang/android/geoquiz/MainActivity.kt
@@ -35,19 +35,11 @@ class MainActivity : AppCompatActivity() {
         questionTextView = findViewById(R.id.question_text_view)
 
         trueButton.setOnClickListener { view: View ->
-            Toast.makeText(
-                this,
-                R.string.correct_toast,
-                Toast.LENGTH_SHORT,
-            ).show()
+            checkAnswer(true)
         }
 
         falseButton.setOnClickListener { view: View ->
-            Toast.makeText(
-                this,
-                R.string.incorrect_toast,
-                Toast.LENGTH_SHORT,
-            ).show()
+            checkAnswer(false)
         }
 
         nextButton.setOnClickListener { view: View ->
@@ -61,5 +53,18 @@ class MainActivity : AppCompatActivity() {
     private fun updateQuestion() {
         val questionTextResId = questionBank[currentIndex].textResId
         questionTextView.setText(questionTextResId)
+    }
+
+    private fun checkAnswer(userAnswer: Boolean) {
+        val correctAnswer = questionBank[currentIndex].answer
+
+        val messageResId = if (userAnswer == correctAnswer) {
+            R.string.correct_toast
+        } else {
+            R.string.incorrect_toast
+        }
+
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT)
+            .show()
     }
 }
