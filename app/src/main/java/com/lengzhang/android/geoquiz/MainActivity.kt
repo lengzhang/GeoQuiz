e32110c557330a1f3ba8ca95378133bc910a7ae5
Leng <lengzhang@live.com>
2021-01-28 13:58:17 -0800

    Wiring up the TextView

--- app/src/main/java/com/lengzhang/android/geoquiz/MainActivity.kt
+++ app/src/main/java/com/lengzhang/android/geoquiz/MainActivity.kt
@@ -4,12 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
+    private lateinit var nextButton: Button
+    private lateinit var questionTextView: TextView
 
     private val questionBank = listOf(
         Question(R.string.question_australia, true),
@@ -28,6 +31,8 @@ class MainActivity : AppCompatActivity() {
 
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
+        nextButton = findViewById(R.id.next_button)
+        questionTextView = findViewById(R.id.question_text_view)
 
         trueButton.setOnClickListener { view: View ->
             Toast.makeText(
@@ -44,5 +49,8 @@ class MainActivity : AppCompatActivity() {
                 Toast.LENGTH_SHORT,
             ).show()
         }
+
+        val questionTextResId = questionBank[currentIndex].textResId
+        questionTextView.setText(questionTextResId)
     }
 }
