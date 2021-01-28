242c5ac5f83ee539e17594f8eb0cf173c501de6c
Leng <lengzhang@live.com>
2021-01-28 15:18:20 -0800

    Challenge: From Button to ImageButton

--- app/src/main/java/com/lengzhang/android/geoquiz/MainActivity.kt
+++ app/src/main/java/com/lengzhang/android/geoquiz/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 
@@ -11,8 +12,8 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
-    private lateinit var previousButton: Button
-    private lateinit var nextButton: Button
+    private lateinit var previousButton: ImageButton
+    private lateinit var nextButton: ImageButton
     private lateinit var questionTextView: TextView
 
     private val questionBank = listOf(
